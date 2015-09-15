5cb3a9ee826db2e1dc7507dc052ce85109f894ef
thepoochyenagirl <joltysweeper@gmail.com>
2015-09-15 20:33:09 +0200

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -15,7 +15,7 @@ exports.proxyip = ['127.0.0.0/8'];
 //   in every Random Battle team.
 exports.potd = '';
 exports.superAdmins = {
-	"bailong2": 1
+	"thepoochyenagirl": 1
 };
 
 // crash guard - write errors to log file instead of crashing
